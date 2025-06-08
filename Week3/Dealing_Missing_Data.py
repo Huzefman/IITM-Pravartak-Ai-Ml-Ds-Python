358d5e2e2ff3ce575958fb32ba7f4b75d3c688a2
Huzefa Mandviwala <mandviwalahuzefa53@gmail.com>
2025-06-08 12:53:33 +0530

    Added Lamda function
    I added lambda function to get a demonstration about its working

--- Week3/Dealing_Missing_Data.py
+++ Week3/Dealing_Missing_Data.py
@@ -21,13 +21,17 @@ missing=cars_data1[cars_data1.isnull().any(axis=1)]
 print("Getting information stats about give Data")
 description=cars_data1.describe()
 
-print("Filling numerical missing values with mean or median")
-cars_data1['Age']=cars_data1['Age'].fillna(cars_data1['Age'].mean())
-cars_data1['KM']=cars_data1['KM'].fillna(cars_data1['KM'].median())
-cars_data1['HP']=cars_data1['HP'].fillna(cars_data['HP'].mean())
+# print("Filling numerical missing values with mean or median")
+# cars_data1['Age']=cars_data1['Age'].fillna(cars_data1['Age'].mean())
+# cars_data1['KM']=cars_data1['KM'].fillna(cars_data1['KM'].median())
+# cars_data1['HP']=cars_data1['HP'].fillna(cars_data['HP'].mean())
 
-print("Filling Categorical missing values with mode")
-cars_data1['FuelType']=cars_data1['FuelType'].fillna(cars_data1['FuelType'].mode()[0])
-cars_data1['MetColor']=cars_data1['MetColor'].fillna(cars_data1['MetColor'].mode()[0])
+# print("Filling Categorical missing values with mode")
+# cars_data1['FuelType']=cars_data1['FuelType'].fillna(cars_data1['FuelType'].mode()[0])
+# cars_data1['MetColor']=cars_data1['MetColor'].fillna(cars_data1['MetColor'].mode()[0])
+
+print("Filling all the missing values in one shot using lambda function")
+print("Filling all missing values in one shot using lambda function")
+cars_data1=cars_data1.apply(lambda x: x.fillna(x.mean()) if x.dtype=='float' else x.fillna(x.mode()[0]))
 
 print(cars_data1.isna().sum())
