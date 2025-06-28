1ad1af600f634eebf44fbd7a31d72fbfbf09b019
Huzefa Mandviwala <mandviwalahuzefa53@gmail.com>
2025-06-28 16:18:50 +0530

    added Data-Imputation files
    I have created files for Understanding of Data-Imputation using advanced concepts of linear algebra, matrices, SVD

---
+++ Data-Imputation/Data-Imputation.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 27 17:02:30 2025
+
+@author: huzefa
+"""
+
+import pandas as pd
+import numpy as np
+import os
+
+# Change working directory to a new path
+os.chdir('/Users/huzefa/IITM-Pravartak-Ai-Ml-Ds-Python/Data-Imputation')
+
+# Verify the change
+print("Current working directory:", os.getcwd())
+
+#Importing Data
+data=pd.read_csv('../DataSets/GTPvar.csv',index_col=0)
+
+#Checking missing values columnwise
+data.isnull().sum(axis=1)
+
+#Creating new variable NApresent to contain all the rows' missing values sum
+data['NApresent']=data.isnull().sum(axis=1)
+
+#Extracting rows with no null values
+df=data[data.NApresent==0]
+
+#Dropping NApresent column from df
+df=df.drop('NApresent',axis=1)
+
+#Converting np to numpy array
+df_mat=df.to_numpy()
+
+#Rank of obtained matrix
+np.linalg.matrix_rank(df_mat)
+
+#SVD decomposition
+v,s,u=np.linalg.svd(df_mat.T)
+
+#Setting tolerance
+tol=1e-8
+
+#Removing columns that are lesser than the tolerance
+rank=min(df_mat.shape)-np.abs(s)[::-1].searchsorted(tol)
+
+#Choosing the null space relation
+A=v[:,rank:]
+A=A.T
+print(A)
+
+#Perfoming transformations to obtain and impute possible missing values
+len(data)
+len(A)  
+  
+for i in range(0,len(data)):
+    if((data.iloc[i,5]==0) | (data.iloc[i,5]>len(A))):
+        continue
+    else:
+        eqnsneeded=data.iloc[i,5]
+        aID=np.empty(0,dtype='int64')
+        bID=np.empty(0,dtype='int64')
+        for j in range(len(data.columns)-1):
+            if(pd.isnull(data.iloc[i,j])):
+                aID=np.append(aID,j)
+            else: bID=np.append(bID,j)
+        a=A[0:eqnsneeded,aID]
+        a=np.array(a)
+        x1=((data.iloc[i,bID].to_numpy()))
+        b2=-A[0:eqnsneeded,bID]
+        b=np.dot(b2,x1)
+        x=np.linalg.solve(a,b)
+        data.iloc[i,aID]=x
